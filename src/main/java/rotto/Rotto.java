e78f3361c86b85cd959a9cc00d672c692d3745a0
mommoo <fk2dhstmzk@>
2019-06-26 09:06:37 +0900

    feat(Rotto) :  - 중복이 없는 랜덤 6자리 숫자 로또 번호 생성 및 리턴 기능 작성.  - 찾을 숫자가 몇개 포함 되어있는지를 구하는 기능 작성.

--- src/main/java/rotto/Rotto.java
+++ src/main/java/rotto/Rotto.java
@@ -2,18 +2,36 @@ package rotto;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Rotto {
+    private static final List<Integer> rottoAllNumbers = createAllNumberOfRotto();
+
+    private final List<Integer> numbers;
 
     Rotto() {
+        this.numbers = createNumbers();
+    }
 
+    private static List<Integer> createAllNumberOfRotto() {
+        return IntStream.range(1,46)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> createNumbers() {
+        Collections.shuffle(rottoAllNumbers);
+        return rottoAllNumbers.subList(0, 6);
     }
 
     public List<Integer> getNumbers() {
-        return Collections.unmodifiableList(Collections.emptyList());
+        return Collections.unmodifiableList(numbers);
     }
 
-    public int computeCountOfNumberExist(List<Integer> numbers) {
-        return -1;
+    public int computeCountOfNumberExist(List<Integer> findNumbers) {
+        return (int) findNumbers.stream()
+                .filter(this.numbers::contains)
+                .count();
     }
 }
