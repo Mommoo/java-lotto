9015ffc565632c86feb64bcb45d153bf11d99578
mommoo <fk2dhstmzk@>
2019-06-26 09:07:22 +0900

    test(RottoTest) :  - 중복이 없는 랜덤 6자리 숫자 로또 생성을 검증하는 코드 작성.  - 포함된 숫자 개수 찾기 기능을 검증하는 코드 작성.

--- src/test/java/rotto/RottoTest.java
+++ src/test/java/rotto/RottoTest.java
@@ -3,6 +3,8 @@ package rotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.List;
@@ -29,12 +31,14 @@ public class RottoTest {
         assertThat(rottoNumbersSet).hasSize(ROTTO_NUMBER_DIGIT);
     }
 
-    @DisplayName("로또 숫자 검색이 반드시 6개가 나와야 한다.")
-    @Test
-    public void testSearchRottoNumbers() {
+    @DisplayName("로또 숫자 검색은 반드시 인자 값 만큼 나와야 한다.")
+    @ParameterizedTest
+    @ValueSource(ints={1,2,3,4,5,6})
+    public void testSearchRottoNumbers(int value) {
         List<Integer> rottoNumbers = rotto.getNumbers();
-        int matchCount = rotto.computeCountOfNumberExist(rottoNumbers);
+        List<Integer> findNumbers = rottoNumbers.subList(0,value);
+        int matchCount = rotto.computeCountOfNumberExist(findNumbers);
 
-        assertThat(matchCount).isEqualTo(ROTTO_NUMBER_DIGIT);
+        assertThat(matchCount).isEqualTo(value);
     }
 }
